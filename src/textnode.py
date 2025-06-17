d60652e4f44bf6e4d2e61f2728d164a55f07ef9e
mfernandez <matiasef90@gmail.com>
2025-06-17 18:44:09 +0000

    text node to html node function

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -25,3 +27,19 @@ class TextNode():
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
+def text_node_to_html_node(textnode):
+    match textnode.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, textnode.text)
+        case TextType.BOLD:
+            return LeafNode("b", textnode.text)
+        case TextType.ITALIC:
+            return LeafNode("i", textnode.text)
+        case TextType.CODE:
+            return LeafNode("code", textnode.text)
+        case TextType.LINK:
+            return LeafNode("a", textnode.text, {"href": textnode.url})
+        case TextType.IMAGE:
+            return LeafNode("image", "", {"src": textnode.url, "alt": textnode.text})
+        case _:
+            raise Exception("non suport text type")
